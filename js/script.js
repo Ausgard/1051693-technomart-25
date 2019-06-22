e23127a9d71bdf43f4cd90bacc332954c88d7036
Serhii Rodik <serhiirodik@gmail.com>
2019-06-22 23:06:43 +0200

    скрипты

--- js/script.js
+++ js/script.js
@@ -1,21 +1,51 @@
-var write_us = document.querySelector("a.mail");
-		open = document.querySelector(".write-us-form");
+var btn = document.querySelector("a.mail");
+		form = document.querySelector(".write-us-form");
 		close = document.querySelector(".close-popup");
-		log = open.querySelector("[name=your-name]");
+		log = form.querySelector("[name=your-name]");
+		mail = form.querySelector("[name=your-mail]"); 
+		text = form.querySelector("[name=comment-field]");
+		isStorageSupport = true;
+		storage = ""; 
 
-		write_us.addEventListener("click", function(evt) {
+		try {
+		storage = localStorage.getItem("log");}
+		catch (err) {
+		isStorageSupport = false;}
+		btn.addEventListener("click", function (evt) {
 			evt.preventDefault();
-			open.classList.add("show-popup");
-			log.focus();
-		});
-		close.addEventListener("click", function(evt) {
-			evt.preventDefault();
-			open.classList.remove("show-popup");
-		})
-		window.addEventListener("keydown", function(evt) {
-			if (evt.keyCode === 27) {
-				if (open.classList.contains("show-popup")) {
-					evt.preventDefault();
-					open.classList.remove("show-popup");
+			form.classList.add("show-popup");
+		if (storage) {
+			log.value = storage;
+			mail.focus();}
+		else {
+			log.focus();}
+		if (storage) {
+			mail.value = storage;
+			text.focus();}
+		else {
+			mail.focus();
+}});
 
+		close.addEventListener("click", function (evt) {
+			evt.preventDefault();
+			form.classList.remove("show-popup");
+			form.classList.remove("error-popup");
+			});
+		form.addEventListener("submit", function (evt) {
+		if (!log.value || !mail.value || !text.value) {
+			evt.preventDefault();
+			form.classList.remove("error-popup");
+			form.offsetWidth = form.offsetWidth;
+			form.classList.add("error-popup");}
+		else {
+		if (isStorageSupport) {
+				localStorage.setItem("log", log.value);
 }}});
+
+window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+		if (form.classList.contains("show-popup")) {
+			evt.preventDefault();
+			form.classList.remove("show-popup");
+			form.classList.remove("error-popup");
+}}}); 
