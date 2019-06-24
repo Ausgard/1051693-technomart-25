50524bf5fde29f2b05dc346d2afc026a395f0b45
Serhii Rodik <serhiirodik@gmail.com>
2019-06-24 19:06:01 +0200

    Попапы

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ var btn = document.querySelector("a.mail");
 		mapBtn = document.querySelector("a.popup-img");
 		mapPopup = document.querySelector(".popup-map");
 		mapClose = document.querySelector(".close-popup-map"); 
+		buyBtn = document.querySelectorAll(".one-click-buy");
+		buyPopup = document.querySelector(".popup-cart");
+		buyClose = document.querySelector(".close-popup-buy");
 
 		isStorageSupport = true;
 		storage = ""; 
@@ -15,6 +18,7 @@ var btn = document.querySelector("a.mail");
 		storage = localStorage.getItem("log");}
 		catch (err) {
 		isStorageSupport = false;}
+		if(btn) {
 		btn.addEventListener("click", function (evt) {
 			evt.preventDefault();
 			form.classList.add("show-popup");
@@ -29,9 +33,10 @@ var btn = document.querySelector("a.mail");
 		else {
 			mail.focus();
 }
-});
+});};
 
-		close.addEventListener("click", function (evt) {
+		if(close) {
+			close.addEventListener("click", function (evt) {
 			evt.preventDefault();
 			form.classList.remove("show-popup");
 			form.classList.remove("error-popup");
@@ -47,7 +52,7 @@ var btn = document.querySelector("a.mail");
 				localStorage.setItem("log", log.value);
 				localStorage.setItem("mail", mail.value);
 				localStorage.setItem("text", text.value);
-}}});
+}}});};
 
 window.addEventListener("keydown", function (evt) {
 		if (evt.keyCode === 27) {
@@ -57,7 +62,8 @@ window.addEventListener("keydown", function (evt) {
 			form.classList.remove("error-popup");
 }}}); 
 
-		mapBtn.addEventListener("click", function (evt) {
+		if(mapBtn) {
+			mapBtn.addEventListener("click", function (evt) {
 			evt.preventDefault();
 			mapPopup.classList.add("show-popup");});
 		mapClose.addEventListener("click", function (evt) {
@@ -68,4 +74,23 @@ window.addEventListener("keydown", function (evt) {
 		if (mapPopup.classList.contains("show-popup")) {
 			evt.preventDefault();
 			mapPopup.classList.remove("show-popup");
-}}});
+}}});};
+
+					[].forEach.call(buyBtn, function(item) {
+    item.addEventListener('click', function(evt) {
+    	evt.preventDefault();
+      buyPopup.classList.add("show-popup");
+    });});
+
+		if(buyClose) {
+			buyClose.addEventListener("click", function (evt) {
+			evt.preventDefault();
+			buyPopup.classList.remove("show-popup");
+			});
+
+		window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+		if (buyPopup.classList.contains("show-popup")) {
+			evt.preventDefault();
+			buyPopup.classList.remove("show-popup");
+}}});};
